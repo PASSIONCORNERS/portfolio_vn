0907afad5a5ee9d5f76bf6322ca033f3c4159a4f
Phong Truong <phong@passioncorners.com>
2021-10-12 16:40:37 +0700

    overlay

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 // Menu
 const menu = document.querySelector(".navbar__links");
 const icon = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
 
 icon.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  icon.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("show");
   menu.classList.toggle("navbar__open");
   icon.classList.toggle("open");
 });
